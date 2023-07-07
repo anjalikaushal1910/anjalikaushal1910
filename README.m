a6f1ff5ed569fd813bfd87e640e523e02ee9a699
anjalikaushal1910 <138889984+anjalikaushal1910@users.noreply.github.com>
2023-07-07 19:38:35 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,29 @@
+#print right half diamond
+n=int(input("enter any no:"))
+for i in range (n+1):
+   c=i*'*'
+   print(c)
+for j in range(n+1,0,-1):
+   d=j*'*'
+   print(d)
+
+#print left half diamond
+
+for i in range(n+1):
+    print(" "*(n-i),"*"*i)
+for i in range(n-1,-1,-1):
+    print(" "*(n-i),"*"*i)
+
+#print 2 right angle triangle at 90degree
+
+for i in range(n+1):
+    print(" "*(n-i),"*"*i)
+for i in range(n,-1,-1):
+    print(" "*(n-1),"*"*i)
+    
+
+#print opposite of upper condition
+for i in range(n+1):
+    print(" "*(n-1),"*"*i)
+for i in range(n,-1,-1):
+    print(" "*(n-i),"*"*i)
